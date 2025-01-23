daac68dc4e8b3453da29736007b5790e51c4147b
afselim <f.ahmed0@gmail.com>
2025-01-23 10:33:22 -0600

    Results on simulated and inferred trees for all demographies

--- data/run_sim_infer.sh
+++ data/run_sim_infer.sh
@@ -8,8 +8,8 @@ demes_file=$1
 deme=$(basename "$demes_file" .${demes_file##*.})
 
 echo "Running method on simulations for $deme"
-../src/run_trasp -input $demes_file -output results/"$deme"_sim -mode simulated -L 10000 -min_time 1 -max_time 30000 -num_timepoints 100 -delta 200
+../src/run_trasp -input $demes_file -output results/"$deme"_sim -mode simulated -L 10000 -min_time 1 -max_time 25000 -num_timepoints 100 -delta 100
 
 echo "Running method on inferred trees for $deme"
 python infer_from_sims.py -input $demes_file -output trees/inferred_from_sims/"$deme" 
-../src/run_trasp -input trees/inferred_from_sims/"$deme" -output results/"$deme"_inferred -metadata trees/inferred_from_sims/"$deme"/metadata.csv  -mode inferred -L 10000 -min_time 1 -max_time 30000 -num_timepoints 100 -delta 200
+../src/run_trasp -input trees/inferred_from_sims/"$deme" -output results/"$deme"_inferred -metadata trees/inferred_from_sims/"$deme"/metadata.csv  -mode inferred -L 10000 -min_time 1 -max_time 25000 -num_timepoints 100 -delta 100
