6dbd57ab36f23bfc520c89b6350706c4025ff818
afselim <f.ahmed0@gmail.com>
2025-01-24 10:59:15 -0600

    updated rate trajectories output

--- data/run_sim_infer.sh
+++ data/run_sim_infer.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+L=10000
+max_time=3000
+
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 demes_file"
@@ -8,8 +11,8 @@ demes_file=$1
 deme=$(basename "$demes_file" .${demes_file##*.})
 
 echo "Running method on simulations for $deme"
-../src/run_trasp -input $demes_file -output results/"$deme"_sim -mode simulated -L 10000 -min_time 1 -max_time 25000 -num_timepoints 100 -delta 100
+../src/run_trasp -input $demes_file -output results/"$deme"_sim -mode simulated -L $L -min_time 1 -max_time $max_time -num_timepoints 100 -delta 100
 
 echo "Running method on inferred trees for $deme"
 python infer_from_sims.py -input $demes_file -output trees/inferred_from_sims/"$deme" 
-../src/run_trasp -input trees/inferred_from_sims/"$deme" -output results/"$deme"_inferred -metadata trees/inferred_from_sims/"$deme"/metadata.csv  -mode inferred -L 10000 -min_time 1 -max_time 25000 -num_timepoints 100 -delta 100
+../src/run_trasp -input trees/inferred_from_sims/"$deme" -output results/"$deme"_inferred -metadata trees/inferred_from_sims/"$deme"/metadata.csv  -mode inferred -L $L -min_time 1 -max_time $max_time -num_timepoints 100 -delta 100
